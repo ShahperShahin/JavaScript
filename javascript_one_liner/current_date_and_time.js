cc02b588a05f4561216ae89696606de813afcead
Shahper Shahin <74124339+ShahperShahin@users.noreply.github.com>
2023-04-23 22:55:40 +0530

    Add files via upload

---
+++ javascript_one_liner/current_date_and_time.js
@@ -0,0 +1,2 @@
+date_time = new Date().toString();
+console.log(date_time)
---
+++ javascript_one_liner/find_max_and_min_in_array.js
@@ -0,0 +1,9 @@
+// 1. to find the max & min value in an array:
+
+// minimum
+let array = Math.min.apply(null,[1, 22, 421, 52, 22, 213, 3, 3, 54, 325, 5, 56, 54, 3, 1]);
+console.log(array)
+
+//maximum
+let array1 = Math.max.apply(null,[1, 22, 421, 52, 22, 213, 3, 3, 54, 325, 5, 56, 54, 3, 1]);
+console.log(array1)
